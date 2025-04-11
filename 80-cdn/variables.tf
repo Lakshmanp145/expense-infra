7f878e0f885580e3bb2c5e44bdf8715115920ede
Lakshmanp1 <laxman.gdn@gmail.com>
2025-04-11 19:10:54 +0530

    fghjk

--- 80-cdn/variables.tf
+++ 80-cdn/variables.tf
@@ -3,13 +3,13 @@ variable "project" {
 }
 
 variable "environment" {
-    default = "dev"
+    default = "prod"
 }
 
 variable "common_tags" {
     default = {
         project = "expense"
-        environment = "dev"
+        environment = "prod"
         terraform = "true"
     }
 }
